2f2400e560c2dee78b0c8db04497dce38da0d108
Sam Mortimer <sam@mortimer.me.uk>
2017-09-21 10:10:49 -0700

    Refactor Ozone init rc file
    *) Put Ozone rc files in /system/etc/init instead of /init.ozone.rc
    
    *) Split into function specific files (vibrator-specific stuff moved to
       our custom vibrator HAL service)
    
    Change-Id: I1d00950253fbf7fdd7a4b7bd52adbed971923575

--- config/common.mk
+++ config/common.mk
@@ -65,9 +65,9 @@ PRODUCT_COPY_FILES += \
     vendor/ozone/prebuilt/common/etc/init.d/90userinit:system/etc/init.d/90userinit
 endif
 
-# Ozone-specific init file
-PRODUCT_COPY_FILES += \
-    vendor/ozone/prebuilt/common/etc/init.local.rc:root/init.ozone.rc
+# Copy all Ozone-specific init rc files
+$(foreach f,$(wildcard vendor/ozone/prebuilt/common/etc/init/*.rc),\
+	$(eval PRODUCT_COPY_FILES += $(f):system/etc/init/$(notdir $f)))
 
 # Copy over added mimetype supported in libcore.net.MimeUtils
 PRODUCT_COPY_FILES += \
