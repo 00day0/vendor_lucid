85c65938a0ac2bca1676b1db3c681c7eda18ad49
Rashed Abdel-Tawab <rashed@linux.com>
2018-08-25 11:10:25 -0700

    lineage: Move QC board variables earlier
    These variables are usually set in a device's BoardConfig, setting them
    in qcom_target is at the very end of the "configuration process" which
    results in them being unavailable to plenty of other configuration
    "things" (ex. soong namespaces or soong config). Move them to right
    after a device's BoardConfig has been found and loaded.
    
    Change-Id: Iddd731202d22ed3f8eb010197ce20d3c75a1f40a

--- config/BoardConfigOzone.mk
+++ config/BoardConfigOzone.mk
@@ -3,7 +3,6 @@ ifeq ($(WITH_OZONE_CHARGER),true)
     BOARD_HAL_STATIC_LIBRARIES := libhealthd.ozone
 endif
 
-# QCOM HW crypto
-ifeq ($(TARGET_HW_DISK_ENCRYPTION),true)
-    TARGET_CRYPTFS_HW_PATH ?= vendor/qcom/opensource/cryptfs_hw
+ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+include vendor/ozone/config/BoardConfigQcom.mk
 endif
