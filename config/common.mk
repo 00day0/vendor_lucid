9a72f7a8428df2afbc4f9d1e3f9aa3642a1d67b7
Christopher N. Hesse <raymanfx@gmail.com>
2018-09-21 23:00:26 +0200

    common: Allow custom vendor to override properties
    Test case: override ro.adb.secure in vendor/extra/product.mk
    Before this patch, the override would not affect the resulting
    property.
    
    Change-Id: I0deb8a6865030b09ed6ba0ede1658319ca033e67

--- config/common.mk
+++ config/common.mk
@@ -1,3 +1,6 @@
+# Allow vendor/extra to override any property by setting it first
+$(call inherit-product-if-exists, vendor/extra/product.mk)
+
 PRODUCT_BRAND ?= Ozone
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
@@ -335,5 +338,3 @@ endif
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 -include vendor/ozone/config/partner_gms.mk
-
-$(call inherit-product-if-exists, vendor/extra/product.mk)
