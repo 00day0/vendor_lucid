a9d708ff96ba2f16ed34d285dd4fd637d82eaebd
Shreyansh Lodha <slodha96@gmail.com>
2018-10-07 22:11:02 +0530

    Add Google Sans and define it's usage
    Change-Id: I204661a3b0378f73d2139f0ac4a94743a3823d24

--- config/packages.mk
+++ config/packages.mk
@@ -1,3 +1,13 @@
 # Include librsjni explicitly to workaround GMS issue
 PRODUCT_PACKAGES += \
     librsjni
+
+PRODUCT_COPY_FILES += \
+   vendor/lucid/prebuilt/common/fonts/GoogleSans-Regular.ttf:system/fonts/GoogleSans-Regular.ttf \
+   vendor/lucid/prebuilt/common/fonts/GoogleSans-Medium.ttf:system/fonts/GoogleSans-Medium.ttf \
+   vendor/lucid/prebuilt/common/fonts/GoogleSans-MediumItalic.ttf:system/fonts/GoogleSans-MediumItalic.ttf \
+   vendor/lucid/prebuilt/common/fonts/GoogleSans-Italic.ttf:system/fonts/GoogleSans-Italic.ttf \
+   vendor/lucid/prebuilt/common/fonts/GoogleSans-Bold.ttf:system/fonts/GoogleSans-Bold.ttf \
+   vendor/lucid/prebuilt/common/fonts/GoogleSans-BoldItalic.ttf:system/fonts/GoogleSans-BoldItalic.ttf
+
+ADDITIONAL_FONTS_FILE := vendor/lucid/prebuilt/common/fonts/google-sans.xml
