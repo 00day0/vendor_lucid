PotatoProject/vendor_potato

Add Google Sans and define it's usage
Change-Id: I204661a3b0378f73d2139f0ac4a94743a3823d24
Signed-off-by: Shreyansh Lodha <slodha96@gmail.com>

config/packages.mk
@@ -37,4 +37,14 @@ PRODUCT_PACKAGES += \
 #    SettingsBlackTheme \
 #    SystemBlackTheme \
 #    SystemSettingsIconTint \
-#    SysuiQsBlackTheme \
+#    SysuiQsBlackTheme 
+
+PRODUCT_COPY_FILES += \
+   vendor/potato/prebuilt/common/fonts/GoogleSans-Regular.ttf:system/fonts/GoogleSans-Regular.ttf \
+   vendor/potato/prebuilt/common/fonts/GoogleSans-Medium.ttf:system/fonts/GoogleSans-Medium.ttf \
+   vendor/potato/prebuilt/common/fonts/GoogleSans-MediumItalic.ttf:system/fonts/GoogleSans-MediumItalic.ttf \
+   vendor/potato/prebuilt/common/fonts/GoogleSans-Italic.ttf:system/fonts/GoogleSans-Italic.ttf \
+   vendor/potato/prebuilt/common/fonts/GoogleSans-Bold.ttf:system/fonts/GoogleSans-Bold.ttf \
+   vendor/potato/prebuilt/common/fonts/GoogleSans-BoldItalic.ttf:system/fonts/GoogleSans-BoldItalic.ttf
+ 
+ADDITIONAL_FONTS_FILE := vendor/potato/prebuilt/common/fonts/google-sans.xml

00day0/vendor_lucid

Signed-off-by: devabhixda <devabhixda@gmail.com>

config/packages.mk
@@ -1,3 +1,13 @@
 # Include librsjni explicitly to workaround GMS issue
 PRODUCT_PACKAGES += \
     librsjni
+
+PRODUCT_COPY_FILES += \
+   vendor/ozone/prebuilt/common/fonts/GoogleSans-Regular.ttf:system/fonts/GoogleSans-Regular.ttf \
+   vendor/ozone/prebuilt/common/fonts/GoogleSans-Medium.ttf:system/fonts/GoogleSans-Medium.ttf \
+   vendor/ozone/prebuilt/common/fonts/GoogleSans-MediumItalic.ttf:system/fonts/GoogleSans-MediumItalic.ttf \
+   vendor/ozone/prebuilt/common/fonts/GoogleSans-Italic.ttf:system/fonts/GoogleSans-Italic.ttf \
+   vendor/ozone/prebuilt/common/fonts/GoogleSans-Bold.ttf:system/fonts/GoogleSans-Bold.ttf \
+   vendor/ozone/prebuilt/common/fonts/GoogleSans-BoldItalic.ttf:system/fonts/GoogleSans-BoldItalic.ttf
+
+ADDITIONAL_FONTS_FILE := vendor/ozone/prebuilt/common/fonts/google-sans.xml