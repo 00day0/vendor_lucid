4eb9c6cdd6e70593f59da97481c77dddf988cc4f
dianlujitao <dianlujitao@lineageos.org>
2019-09-06 10:46:50 +0800

    Fix real file depends on PHONY target
    Change-Id: Idd42f4575dfd9fbbff9ab15e20e65d932df0390c

--- build/tasks/apicheck.mk
+++ build/tasks/apicheck.mk
@@ -38,7 +38,7 @@ checkapi-ozone : check-ozone-public-api
 # (See ozone-sdk/Android.mk)
 # we need to add api-stubs as additional dependency of the api check.
 
-$(INTERNAL_OZONE_PLATFORM_API_FILE): ozone-api-stubs-docs
+$(INTERNAL_OZONE_PLATFORM_API_FILE):
 
 # Check that the API we're building hasn't broken the last-released
 # SDK version.
