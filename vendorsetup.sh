ebe82d1484abeb8ff4da1c5a312de4f24f311325
Rashed Abdel-Tawab <rashed@linux.com>
2019-09-06 17:35:16 -0700

    ozone: Remove deprecated vendorsetup.sh
    Change-Id: I13d6b98e9520e8f61b261ffdf171c11624303122

--- vendorsetup.sh
+++
@@ -1,4 +0,0 @@
-for combo in $(curl -s https://raw.githubusercontent.com/OzoneOS/hudson/master/ozone-build-targets | sed -e 's/#.*$//' | grep ozone-15.0 | awk '{printf "ozone_%s-%s\n", $1, $2}')
-do
-    add_lunch_combo $combo
-done
