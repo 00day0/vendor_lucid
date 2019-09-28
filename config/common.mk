LineageOS/android_vendor_cm

config: Add new prop for major.minor build version
This only include the major and minor version as returned
by the updater.

Change-Id: I4b203fcddb9ab60958063906c3ed4aa6f0607277

config/common.mk
@@ -316,6 +316,7 @@ endif
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.cm.version=$(LINEAGE_VERSION) \
     ro.cm.releasetype=$(CM_BUILDTYPE) \
+    ro.cm.build.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
     ro.modversion=$(LINEAGE_VERSION) \
     ro.cmlegal.url=https://lineageos.org/legal
 

00day0/vendor_lucid

config/common.mk
@@ -310,6 +310,7 @@ endif
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.ozone.version=$(OZONE_VERSION) \
+    ro.ozone.build.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
     ro.ozone.releasetype=$(OZONE_BUILDTYPE) \
     ro.modversion=$(OZONE_VERSION) \
     ro.ozonelegal.url=https://ozoneos.org/legal