f08d27c0b2481a65da857f00e0bcd8bc670f47ba
devabhixda <devabhixda@gmail.com>
2019-09-28 20:37:08 +0530

    build: soong: put ozone variables in the Ozone struct
    Signed-off-by: devabhixda <devabhixda@gmail.com>

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,5 +1,8 @@
+ # Insert new variables inside the Ozone structure
 ozone_soong:
 	$(hide) mkdir -p $(dir $@)
 	$(hide) (\
 	echo '{'; \
+	echo '"Ozone": {'; \
+	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
