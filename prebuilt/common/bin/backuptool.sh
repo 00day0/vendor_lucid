LineageOS/android_vendor_lineage

backuptool: Take into account new location for system default props
 * System default props defined using PRODUCT_SYSTEM_DEFAULT_PROPERTIES
   are stored into /system/etc/prop.default, so that's the location where
   ro.lineage.version prop needs to be checked now. Although, fallback
   to the old location to allow sucessful upgrades.

Change-Id: I62046447876c2198a0c4f88a4f36f4723d417617

prebuilt/common/bin/backuptool.sh
@@ -34,7 +34,7 @@ check_prereq() {
 if [ ! -r /system/build.prop ]; then
     return 0
 fi
-if ( ! grep -q "^ro.lineage.version=$V.*" /system/build.prop ); then
+if [ ! grep -q "^ro.lineage.version=$V.*" /system/etc/prop.default /system/build.prop ]; then
   echo "Not backing up files from incompatible version: $V"
   return 0
 fi

00day0/vendor_lucid

prebuilt/common/bin/backuptool.sh
@@ -34,7 +34,7 @@ check_prereq() {
 if [ ! -r /system/build.prop ]; then
     return 0
 fi
-if ( ! grep -q "^ro.ozone.version=$V.*" /system/build.prop ); then
+if [ ! grep -q "^ro.ozone.version=$V.*" /system/etc/prop.default /system/build.prop ]; then
   echo "Not backing up files from incompatible version: $V"
   return 0
 fi