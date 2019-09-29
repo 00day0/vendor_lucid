LineageOS/android_vendor_lineage

lineage: Append time of day to zip names for unofficials
* Helpful when building multiple builds in a day

Change-Id: Ia0096ea5fbc3e479a549b7805523b2b06cb223da

config/common.mk
@@ -308,9 +308,17 @@ ifeq ($(LINEAGE_BUILDTYPE), RELEASE)
     endif
 else
     ifeq ($(LINEAGE_VERSION_MAINTENANCE),0)
-        LINEAGE_VERSION := $(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date -u +%Y%m%d)-$(LINEAGE_BUILDTYPE)$(LINEAGE_EXTRAVERSION)-$(LINEAGE_BUILD)
+        ifeq ($(LINEAGE_BUILDTYPE), UNOFFICIAL)
+            LINEAGE_VERSION := $(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date -u +%Y%m%d_%H%M%S)-$(LINEAGE_BUILDTYPE)$(LINEAGE_EXTRAVERSION)-$(LINEAGE_BUILD)
+        else
+            LINEAGE_VERSION := $(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date -u +%Y%m%d)-$(LINEAGE_BUILDTYPE)$(LINEAGE_EXTRAVERSION)-$(LINEAGE_BUILD)
+        endif
     else
-        LINEAGE_VERSION := $(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR).$(LINEAGE_VERSION_MAINTENANCE)-$(shell date -u +%Y%m%d)-$(LINEAGE_BUILDTYPE)$(LINEAGE_EXTRAVERSION)-$(LINEAGE_BUILD)
+        ifeq ($(LINEAGE_BUILDTYPE), UNOFFICIAL)
+            LINEAGE_VERSION := $(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR).$(LINEAGE_VERSION_MAINTENANCE)-$(shell date -u +%Y%m%d_%H%M%S)-$(LINEAGE_BUILDTYPE)$(LINEAGE_EXTRAVERSION)-$(LINEAGE_BUILD)
+        else
+            LINEAGE_VERSION := $(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR).$(LINEAGE_VERSION_MAINTENANCE)-$(shell date -u +%Y%m%d)-$(LINEAGE_BUILDTYPE)$(LINEAGE_EXTRAVERSION)-$(LINEAGE_BUILD)
+        endif
     endif
 endif
 

00day0/vendor_lucid

config/common.mk
@@ -298,9 +298,17 @@ ifeq ($(OZONE_BUILDTYPE), RELEASE)
     endif
 else
     ifeq ($(OZONE_VERSION_MAINTENANCE),0)
-        OZONE_VERSION := $(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date -u +%Y%m%d)-$(OZONE_BUILDTYPE)$(OZONE_EXTRAVERSION)-$(OZONE_BUILD)
+        ifeq ($(OZONE_BUILDTYPE), UNOFFICIAL)
+            OZONE_VERSION := $(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date -u +%Y%m%d_%H%M%S)-$(OZONE_BUILDTYPE)$(OZONE_EXTRAVERSION)-$(OZONE_BUILD)
+        else
+            OZONE_VERSION := $(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date -u +%Y%m%d)-$(OZONE_BUILDTYPE)$(OZONE_EXTRAVERSION)-$(OZONE_BUILD)
+        endif
     else
-        OZONE_VERSION := $(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR).$(OZONE_VERSION_MAINTENANCE)-$(shell date -u +%Y%m%d)-$(OZONE_BUILDTYPE)$(OZONE_EXTRAVERSION)-$(OZONE_BUILD)
+        ifeq ($(OZONE_BUILDTYPE), UNOFFICIAL)
+            OZONE_VERSION := $(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR).$(OZONE_VERSION_MAINTENANCE)-$(shell date -u +%Y%m%d_%H%M%S)-$(OZONE_BUILDTYPE)$(OZONE_EXTRAVERSION)-$(OZONE_BUILD)
+        else
+            OZONE_VERSION := $(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR).$(OZONE_VERSION_MAINTENANCE)-$(shell date -u +%Y%m%d)-$(OZONE_BUILDTYPE)$(OZONE_EXTRAVERSION)-$(OZONE_BUILD)
+        endif
     endif
 endif
 